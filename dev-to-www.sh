2d4489d17f3d964136c093888f6852f1bf69f44d
Martin <github@diemattels.at>
2020-06-27 19:44:12 +0200

    Add files via upload

---
+++ dev-to-www.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+SRC='dev.innovation-und-beratung.com';
+DST='www.innovation-und-beratung.com';
+find .  -type d \( \
+         -name "wp-snapshots" -o\
+         -name "wp-includes" -o \
+         -name "wp-admin" -o \
+         -name "cache" -o \
+         -name "et-cache" -o \
+         -name "sitemap-cache" -o \
+         -name "uploads" -o \
+         -name "languages" -o\
+         -name "plugins" -o\
+         -name "cache" -o \
+         -name "Divi" \) \
+         -prune -o \
+        -type f -not \( -iname '*.sh' -o \
+         -iname '*.js' -o \
+         -iname '*.mp4' -o \
+         -iname '*.jpg' -o \
+         -iname '*.png' -o \
+         -iname '401.html' \) \
+        -exec grep -q "$SRC" {} \; \
+        -exec sed -i 's,'"$SRC"','"$DST"',' {} \; \
+        -printf '"%p"\n'
