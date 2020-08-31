d7dd32a9422126ead0f115e3abe04fdc61f7e766
Martin <github@diemattels.at>
2020-08-31 14:37:55 +0200

    Update dev-to-www.sh

--- dev-to-www.sh
+++ dev-to-www.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 SRC='dev.innovation-und-beratung.com';
 DST='www.innovation-und-beratung.com';
+
+printf "\n Replace URL in files \n"
+
 find .  -type d \( \
          -name "wp-snapshots" -o\
          -name "wp-includes" -o \
@@ -31,3 +34,13 @@ wp site switch-language de_DE
 printf "\n Encourage search engines from indexing this site \n"
 
 wp option set blog_public 1
+
+printf "\n Replacing $SRC to $DST"
+# https://developer.wordpress.org/cli/commands/search-replace/
+wp search-replace $SRC $DST --all-tables --report-changed-only # --dry-run
+
+printf "\n Flushing Cache"
+wp cache flush
+
+printf "\n Rebuild W3TC Cache"
+wp w3-total-cache pgcache_prime
